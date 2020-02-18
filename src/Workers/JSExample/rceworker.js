2869dc12d7db424ca31368535bbbe2849f931964
spanishprisoner <jacek.mowinski@gmail.com>
2020-02-18 20:08:06 +0100

    Registration

--- src/Workers/JSExample/rceworker.js
+++ src/Workers/JSExample/rceworker.js
@@ -1,9 +1,23 @@
 var rceWorker = {};
 (async _rceWorker => {
-    class RegistrationState {
-        async handle(jobRunnerStateMachine) {
-            console.log('Registering worker');
-        }
+    let baseUrl = 'http://localhost:3140';
+
+    class UrlSuffixes {
+        static get registerAddressSuffix() {
+            return '/api/workers/register/'
+        };
+        static get getJobAddressSuffix() {
+            return '/api/workers/WORKER_ID/jobs/'
+        };
+        static get updateJobAddressSuffix() {
+            return '/api/workers/WORKER_ID/jobs/JOB_ID/update'
+        };
+        static get completeJobAddressSuffix() {
+            return '/api/workers/WORKER_ID/jobs/JOB_ID/complete'
+        };
+        static get closeWorkerAddressSuffix() {
+            return '/api/workers/WORKER_ID/close'
+        };
     }
 
     class JobRunnerStateMachine {
@@ -16,7 +30,7 @@ var rceWorker = {};
             this.state = new RegistrationState();
             while (this.stopped === false) {
                 await this.state.handle(this);
-                await new Promise(e => setTimeout(e, 1000));
+                await new Promise(e => setTimeout(e, 10000));
             }
         }
         async stop() {
@@ -25,6 +39,39 @@ var rceWorker = {};
         }
         async closeWorker() {
             //todo
+        }
+    }
+
+    class RegistrationState {
+        registrationModel = {
+            name: 'Counter',
+            description: 'JS Worker',
+            //base64Logo: '',
+            jobDescriptions: [{
+                name: 'Count',
+                description: [
+                    'Counts in one second intervals. Params:',
+                    ' - from [int]',
+                    ' - to [int]'
+                ],
+                defaultPayload: {
+                    'from': 0,
+                    'to': 5
+                }
+            }]
+        };
+		
+        async handle(jobRunnerStateMachine) {
+            console.log('Registering worker');
+            const url = baseUrl + UrlSuffixes.registerAddressSuffix;
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.registrationModel)
+            }).then(e => e.text());
+            console.log(response);
         }
     }
 
