9032dab5fb147718c0bf371cb5eca7718edc8c98
spanishprisoner <jacek.mowinski@gmail.com>
2020-08-09 17:40:24 +0200

    Adding NavBar.razor

---
+++ src/Server/RceServer.Front.Blazor/Data/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RceServer.Front.Blazor.Data
+{
+	public class AuthService
+	{
+		public event Func<Task> OnChanged;
+		public bool UserAuthenticated { get; set; }
+
+		public AuthService()
+		{
+			Task.Run(Test);
+		}
+
+		public async Task Test()
+		{
+			while (true)
+			{
+				await Task.Delay(1000);
+				UserAuthenticated = !UserAuthenticated;
+				OnChanged?.Invoke();
+			}
+		}
+	}
+}
--- src/Server/RceServer.Front.Blazor/Startup.cs
+++ src/Server/RceServer.Front.Blazor/Startup.cs
@@ -29,7 +29,8 @@ namespace RceServer.Front.Blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-        }
+            services.AddScoped<AuthService>();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
