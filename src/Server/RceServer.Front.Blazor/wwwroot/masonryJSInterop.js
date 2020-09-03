4845848a029025ccbeaa613bd1fc0d5f73fc6dc2
spanishprisoner <jacek.mowinski@gmail.com>
2020-09-03 19:58:17 +0200

    wip: masonry testing

---
+++ src/Server/RceServer.Front.Blazor/wwwroot/masonryJSInterop.js
@@ -0,0 +1,12 @@
+﻿function masonryReinitialize() {
+	m = new Masonry('.grid', { fitWidth: true });
+}
+
+function masonryRedraw() {
+	var x = document.getElementsByClassName("addtomasonry");
+	for (i = x.length; i > 0; i--) {
+		m.appended([x[i - 1]]);
+		x[i-1].classList.remove("addtomasonry");
+	}
+	m.layout();
+}
