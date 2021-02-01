2788b35410b0f66a4e461c1f6d6ba6328d7ed842
spanishprisoner <jacek.mowinski@gmail.com>
2021-02-01 21:48:30 +0100

    Adding dependency to RceSharpLib

---
+++ src/Workers/RpzwTimer/RceSharpLib/JobRunnerContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RceSharpLib
+{
+	public class JobRunnerContext
+	{
+		public string BaseUrl { get; set; }
+		public List<string> Owners { get; set; }
+		public bool RunInParallel { get; set; }
+		public Dictionary<string, Type> JobHandlerDictionary { get; set; }
+		public RegistrationModel RegistrationModel { get; set; }
+	}
+}
