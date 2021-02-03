36a14c95140a6dd802780472287b576568784e3a
spanishprisoner <jacek.mowinski@gmail.com>
2021-02-03 19:31:50 +0100

    Gpio test

--- src/Workers/RpzwTimer/RpzwTimer/Program.cs
+++ src/Workers/RpzwTimer/RpzwTimer/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading.Tasks;
 
 namespace RpzwTimer
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			var gpioController = new GpioController();
+			gpioController.OpenPin(21, PinMode.Output);
+
+			try
+			{
+				while (true)
+				{
+					gpioController.Write(21, PinValue.High);
+					await Task.Delay(1000);
+					gpioController.Write(21, PinValue.Low);
+					await Task.Delay(1000);
+				}
+			}
+			finally
+			{
+				gpioController.ClosePin(21);
+			}
 		}
 	}
 }
