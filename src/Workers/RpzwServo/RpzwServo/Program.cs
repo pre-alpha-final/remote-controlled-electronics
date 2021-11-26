af3c71e383293b48a21db9d6dc0fc8d2f115d08b
spanishprisoner <jacek.mowinski@gmail.com>
2021-11-26 01:43:30 +0100

    Demo movement

--- src/Workers/RpzwServo/RpzwServo/Program.cs
+++ src/Workers/RpzwServo/RpzwServo/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Device.Pwm;
+using System.Threading.Tasks;
 
 namespace RpzwServo
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			PwmChannel pwmChannel = PwmChannel.Create(0, 0, 50, 0);
+			pwmChannel.Start();
+
+			while (true)
+			{
+				for (var i = 0.035; i < 0.11; i += 0.002)
+				{
+					pwmChannel.DutyCycle = i;
+					Console.WriteLine(i);
+					await Task.Delay(100);
+				}
+			}
+
+			pwmChannel.Stop();
 		}
 	}
 }
