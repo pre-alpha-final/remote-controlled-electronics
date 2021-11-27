ecf484f24bd647ca34cdb346e1e50bbd911d2c01
spanishprisoner <jacek.mowinski@gmail.com>
2021-11-27 03:25:26 +0100

    Finalizing servo demo

---
+++ src/Workers/RpzwServo/RpzwServo/Handlers/StartHandler.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using RceSharpLib;
+using RceSharpLib.JobHandlers;
+using RpzwServo.Services;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RpzwServo.Handlers
+{
+	public class StartHandler : JobHandlerBase
+	{
+		public StartHandler(string baseUrl, Job job) : base(baseUrl, job)
+		{
+		}
+
+		public override JobDescription JobDescription => new JobDescription
+		{
+			Name = "Start moving",
+			Description = new List<string>
+			{
+				"Starts moving back and forth"
+			},
+			DefaultPayload = JObject.Parse(@"{}")
+		};
+
+		public override async Task Handle(CancellationToken cancellationToken)
+		{
+			ServoService.Instance.Start();
+			await CompleteJob(new { jobStatus = Statuses.Success.ToString() });
+		}
+	}
+}
---
+++ src/Workers/RpzwServo/RpzwServo/Services/ServoService.cs
@@ -0,0 +1,86 @@
+﻿using System.Device.Pwm;
+using System.Threading.Tasks;
+
+namespace RpzwServo.Services
+{
+	public class ServoService
+	{
+		private const double StartPosition = 0.035;
+		private const double StopPosition = 0.11;
+		private const double Step = 0.0005;
+
+		private bool _moving;
+		private bool _movingClockwise;
+		private double _position = StartPosition;
+
+		private static ServoService _instance;
+		public static ServoService Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new ServoService();
+				}
+				return _instance;
+			}
+		}
+
+		public void Initialize()
+		{
+			PwmChannel pwmChannel = PwmChannel.Create(0, 0, 50, StartPosition);
+			pwmChannel.Start();
+			Task.Run(() => Loop(pwmChannel));
+		}
+
+		public void Start()
+		{
+			_moving = true;
+		}
+
+		public void Stop()
+		{
+			_moving = false;
+		}
+
+		public async Task Loop(PwmChannel pwmChannel)
+		{
+			while (true)
+			{
+				if (_moving)
+				{
+					if (_movingClockwise)
+					{
+						_position -= Step;
+						if (_position <= StartPosition)
+						{
+							_movingClockwise = false;
+						}
+					}
+					else
+					{
+						_position += Step;
+						if (_position >= StopPosition)
+						{
+							_movingClockwise = true;
+						}
+					}
+					pwmChannel.DutyCycle = _position;
+				}
+				else
+				{
+					if (_position > StartPosition)
+					{
+						_position -= Step;
+						pwmChannel.DutyCycle = _position;
+					}
+					else
+					{
+						pwmChannel.DutyCycle = 0;
+					}
+				}
+				await Task.Delay(25);
+			}
+		}
+	}
+}
