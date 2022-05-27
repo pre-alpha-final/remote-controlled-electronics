78afa488dff243cb6d8a399adbe96a9a65d72554
pre-alpha-final <jacek.mowinski@gmail.com>
2022-05-27 17:44:02 +0200

    Framework change

---
+++ src/Workers/RpzwRelay/RceSharpLib/JobHandlers/JobHandlerBase.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RceSharpLib.JobHandlers
+{
+	public abstract class JobHandlerBase
+	{
+		private readonly string _baseUrl;
+		protected Job Job { get; set; }
+		public abstract JobDescription JobDescription { get; }
+		public abstract Task Handle(CancellationToken cancellationToken);
+
+		public JobHandlerBase(string baseUrl, Job job)
+		{
+			_baseUrl = baseUrl;
+			Job = job;
+		}
+
+		protected async Task UpdateJob(object payload)
+		{
+			Console.WriteLine($"Updating job: '{Job?.JobName}' '{Job?.JobId}'");
+
+			try
+			{
+				var updateJobAddressSuffix = Consts.UpdateJobAddressSuffix
+					.Replace("WORKER_ID", Job.WorkerId.ToString())
+					.Replace("JOB_ID", Job.JobId.ToString());
+				var requestUri = $"{_baseUrl}{updateJobAddressSuffix}";
+
+				using (var client = new HttpClient())
+				using (await client.PostAsync(requestUri, new StringContent(
+					JsonConvert.SerializeObject(payload),
+					Encoding.UTF8,
+					"application/json")))
+				{
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Updating job failed: '{Job?.JobName}' '{Job?.JobId}' '{e.Message}'");
+			}
+		}
+
+		protected async Task CompleteJob(object payload)
+		{
+			Console.WriteLine($"Completing job: '{Job?.JobName}' '{Job?.JobId}'");
+
+			try
+			{
+				var completeJobAddressSuffix = Consts.CompleteJobAddressSuffix
+					.Replace("WORKER_ID", Job.WorkerId.ToString())
+					.Replace("JOB_ID", Job.JobId.ToString());
+				var requestUri = $"{_baseUrl}{completeJobAddressSuffix}";
+
+				using (var client = new HttpClient())
+				using (await client.PostAsync(requestUri, new StringContent(
+					JsonConvert.SerializeObject(payload),
+					Encoding.UTF8,
+					"application/json")))
+				{
+				}
+			}
+			catch (Exception e)
+			{
+				await FailJob(e.Message);
+				Console.WriteLine($"Completing job failed: '{Job?.JobName}' '{Job?.JobId}' '{e.Message}'");
+			}
+		}
+
+		protected async Task FailJob(string reason)
+		{
+			Console.WriteLine($"Job failed: '{Job?.JobName}' '{Job?.JobId}' '{reason}'");
+
+			try
+			{
+				var completeJobAddressSuffix = Consts.CompleteJobAddressSuffix
+					.Replace("WORKER_ID", Job.WorkerId.ToString())
+					.Replace("JOB_ID", Job.JobId.ToString());
+				var requestUri = $"{_baseUrl}{completeJobAddressSuffix}";
+
+				using (var client = new HttpClient())
+				using (await client.PostAsync(requestUri, new StringContent(
+					JsonConvert.SerializeObject(new
+					{
+						failure = reason,
+						jobStatus = Statuses.Failure.ToString()
+					}),
+					Encoding.UTF8,
+					"application/json")))
+				{
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Reporting job failure failed: '{Job?.JobName}' '{Job?.JobId}' '{e.Message}'");
+			}
+		}
+	}
+}
---
+++ src/Workers/RpzwRelay/RceSharpLib/States/RunJobsState.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using RceSharpLib.JobHandlers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RceSharpLib.States
+{
+	internal class RunJobsState : StateBase, IState
+	{
+		private List<Job> _jobs;
+
+		public RunJobsState(StateBase previousState, List<Job> jobs)
+			: base(previousState)
+		{
+			_jobs = jobs;
+		}
+
+		public async Task Handle()
+		{
+			foreach (var job in _jobs)
+			{
+				if (RceJobRunner.JobRunnerContext.JobHandlerDictionary.TryGetValue(job.JobName, out var jobHandlerType) == false)
+				{
+					_ = Task.Run(() => FailJob(job, $"No job named '{job?.JobName}'"));
+					continue;
+				}
+
+				Console.WriteLine($"Running job: '{job?.JobName}' '{job?.JobId}'");
+				try
+				{
+					var jobHandler = (JobHandlerBase)Activator.CreateInstance(jobHandlerType, new object[] { RceJobRunner.JobRunnerContext.BaseUrl, job });
+					if (RceJobRunner.JobRunnerContext.RunInParallel)
+					{
+						_ = Task.Run(() => jobHandler.Handle(RceJobRunner.CancellationTokenSource.Token));
+					}
+					else
+					{
+						await jobHandler.Handle(RceJobRunner.CancellationTokenSource.Token);
+					}
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"Running job failed: '{job?.JobName}' '{job.JobId}' '{e.Message}'");
+				}
+			}
+
+			RceJobRunner.State = new GetJobsState(this);
+		}
+
+		private async Task FailJob(Job job, string reason)
+		{
+			Console.WriteLine($"Job failed: '{job?.JobName}' '{job?.JobId}' '{reason}'");
+
+			try
+			{
+				var completeJobAddressSuffix = Consts.CompleteJobAddressSuffix
+					.Replace("WORKER_ID", job.WorkerId.ToString())
+					.Replace("JOB_ID", job.JobId.ToString());
+				var requestUri = $"{RceJobRunner.JobRunnerContext.BaseUrl}{completeJobAddressSuffix}";
+
+				using (var client = new HttpClient())
+				using (await client.PostAsync(requestUri, new StringContent(
+					JsonConvert.SerializeObject(new
+					{
+						failure = reason,
+						jobStatus = Statuses.Failure.ToString()
+					}),
+					Encoding.UTF8,
+					"application/json")))
+				{
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Reporting job failure failed: '{job?.JobName}' '{job?.JobId}' '{e.Message}'");
+			}
+		}
+	}
+}
